4144db6edd7633d25ec7b3b844e55a4cc5fe0ca6
Eric Bock <eric@techtirade.de>
2021-02-22 13:54:54 +0100

    changed data collection to regex, counting username and port

--- stats.py
+++ stats.py
@@ -1,15 +1,39 @@
 import re
 
 invalid_logins = []
+users = []
+ip = []
+ports = []
+user_dict = {}
+port_dict = {}
 
-with open("auth.log.1", 'r') as file:
+with open("auth.log.testfile", 'r') as file:
     for line in file:
         match = re.search(r'Invalid\suser\s\w+\sfrom\s\d+\.\d+.\d+\.\d+\sport\s\d+', line)
         if match:
             invalid_logins.append(match)
 
 for line in invalid_logins:
-    splitline = line.string.split(" ")
-    user = splitline[7]
-    ip = splitline[9]
-    port = splitline[11]
+    # USER
+    match = re.search("user\s\w+", line.string)
+    matchstring = match.group(0)
+    user = matchstring.split(" ")[1]
+    users.append(user)
+
+    # IP
+    match = re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line.string)
+    ip.append(match.group(0))
+
+    # PORTS (It is done this way so we don't have a bug in which PORT is part of the port dict)
+    match = re.search("port \d+", line.string)
+    matchstring = match.group(0)
+    port = matchstring.split(" ")[1]
+    ports.append(port)
+
+# TODO: INFO: Root doesn't appear in this list, why?
+# TODO: find way to automate lookup of ip origin
+for user in users:
+    user_dict[user] = users.count(user)
+
+for port in ports:
+    port_dict[port.strip('\n')] = ports.count(port)
