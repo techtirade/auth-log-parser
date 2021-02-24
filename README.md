2c71386b9d4a048af794efc2a634aace0b579d21
Eric Bock <eric@techtirade.de>
2021-02-24 01:00:23 +0100

    implemented ip lookup and plotting

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Auth log parser
 Little project to eventually visualize what the most scanned users are which bots use to try to access my ssh server.
 
+Source: several rotated auth.log files from my public facing server
+
 ## Commonly used usernames in ssh probes
+usernames that are probed more than 20 times in this case.
 ![bar chart of usernames](/img/usernames.png)
+
+## Most common country in ssh probe ips
+countries with more than 22 hits in this case
+![bar chart of origin](/img/origin.png)
+
+## Next steps
+- implement argparse to pass auth.log files into the script
+- cache data from ip lookup if it is the same file
--- stats.py
+++ stats.py
@@ -1,11 +1,16 @@
 import re
+import requests
 import matplotlib.pyplot as plt
 import pandas as pd
+from time import sleep
 
 invalid_logins = []
 users = []
 user_dict = {}
-ip = []
+ip_list = []
+ip_dict = {}
+countries = []
+country_dict = {}
 
 with open("auth.log.testfile", 'r') as file:
     for line in file:
@@ -22,12 +27,54 @@ for line in invalid_logins:
 
     # IP
     match = re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line.string)
-    ip.append(match.group(0))
+    ip_list.append(match.group(0))
+
+# ip location check
+# via https://ip-api.com/docs/api:batch
 
 # TODO: INFO: Root doesn't appear in this list, why?
-# TODO: find way to automate lookup of ip origin
+clean_ip_list = list(dict.fromkeys(ip_list))
+clean_ip_list_chunks = [clean_ip_list[i:i + 99] for i in range(0, len(clean_ip_list), 99)]
+data = []
+print("IP Lookups ongoing")
+for chunk in clean_ip_list_chunks:
+    for ip in chunk:
+        data.append({"query": "{}".format(ip)})
+
+    req = requests.post('http://ip-api.com/batch', json=data)
+    if req.ok:
+        for ipinfos in req.json():
+            countries.append(ipinfos.get('country'))
+    data.clear()
+
+for country in countries:
+    country_dict[country] = countries.count(country)
+
 for user in users:
     user_dict[user] = users.count(user)
+
+
+# Plot origin countries
+origin = []
+origin_occurences = []
+
+for k, v in country_dict.items():
+    if v > 22:
+        origin.append(k)
+        origin_occurences.append(v)
+
+print("Creating plot for ip origins")
+df = pd.DataFrame({"origin":origin,"origin_occurences":origin_occurences})
+df_sorted = df.sort_values('origin_occurences',ascending=False)
+
+fig = plt.figure()
+plt.bar('origin', 'origin_occurences',data=df_sorted)
+plt.xlabel('origin')
+plt.ylabel('occurences')
+plt.xticks(rotation = 45)
+plt.title('Common origins of the bot probes')
+plt.gcf().subplots_adjust(bottom=0.20)
+plt.savefig("./img/origin.png")
 
 # plot users that are used more than 20 times
 cleanuser = []
@@ -37,6 +84,7 @@ for k, v in user_dict.items():
         cleanuser.append(k)
         occurences.append(v)
 
+print("Creating plot for login users")
 df = pd.DataFrame({"usernames":cleanuser,"occurences":occurences})
 df_sorted = df.sort_values('occurences',ascending=False)
 
